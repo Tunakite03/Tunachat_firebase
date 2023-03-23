df99ce2c3ed2e9a2365d69a26d8ec39c1a7e237f
tunakite <113432700+Tunakite03@users.noreply.github.com>
2023-03-23 23:43:25 +0700

    Add files via upload

---
+++ src/pages/Login.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
+import Swal from 'sweetalert2'
+
+const Login = () => {
+  const [err, setErr] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Login success',
+        showConfirmButton: false,
+        timer: 1000
+      })
+      navigate("/")
+    } catch (err) {
+      setErr(err.message);
+
+    }
+  };
+  return (
+    <div className="formContainer">
+      <div className="formWrapper">
+        <span className="logo">Lama Chat</span>
+        <span className="title">Login</span>
+        <form onSubmit={handleSubmit}>
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <button>Sign in</button>
+          {<span style={{ color: "red" }}>{err}</span>}
+        </form>
+        <p>You don't have an account? <Link to="/register">Register</Link></p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
